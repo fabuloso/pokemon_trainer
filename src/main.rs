39b23606667a88ebb297fe2c5b01b1f338a80d5b
fabuloso <dalfovo.alessandro@gmail.com>
2021-09-10 09:30:33 +0200

    Introduce a fake query request on pokemons

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,13 @@ fn main() {
     let command_service = Arc::new(Mutex::new(CommandService::new()));
     rouille::start_server("localhost:4567", move |request| {
         router!(request,
-        (POST)(/pokemons) => {
+        (POST) (/pokemons) => {
             let data : Payload = try_or_400!(json_input(request));
             let command_result = command_service.lock().unwrap().execute(data);
             rouille::Response::json(&command_result)
+        },
+        (GET) (/pokemons/{id : u32}) => {
+            rouille::Response::text("ciao")
         },
         _ => {
             rouille::Response::empty_400()
